bc4b2d3472397ddb1c71b69bd73649564747e710
Rishi Panthee <rishipanthee@hotmail.com>
2019-08-07 20:24:03 -0500

    Get all of user's balance

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ var SSC = require("sscjs")
 const ssc = new SSC('https://api.steem-engine.com/rpc')
 
 /**
- * Get user's balance for a token
+ * Get user's balance for a token.
  * @param {String} account The account to get balance from.
  * @param {String} symbol Symbol of token to get balance of.
  * @param {Object} callback Callback. Check if success is true to see if sending worked.
@@ -18,7 +18,43 @@ function getUserTokenBalance(account, symbol, callback){
     })
 }
 
+/**
+ * Get all of user's balances.
+ * @param {String} account The account to get balance from.
+ * @param {Object} callback Callback. Check if success is true to see if sending worked.
+ */
+function getUserAllTokenBalances(account, callback){
+    makeUserWholeTokenBalance(account, 0, [], 0, (res) => {
+        if (callback){
+            callback(res)
+        }
+    })
+}
+
+function makeUserWholeTokenBalance(account, start, balance, errorCount, callback){
+    ssc.find('tokens','balances', { account: account}, 1000, start, [], (err, result) => {
+        if (err) {
+            if (errorCount > 3){
+                return callback({success : false, err : err})
+            }
+            setTimeout(() => {
+                errorCount = errorCount + 1
+                makeUserWholeTokenBalance(account, start, balance, callback)
+            }, 1000 * 0.5)
+        } else {
+            for (i in result){
+                balance.push(result[i])
+            }
+            if (result.length == 1000){
+                makeUserWholeTokenBalance(account, start + 1000, balance, callback)
+            } else {
+                callback({success : true, data : balance})
+            }
+        }
+    })
+}
 
 module.exports = {
-    getUserTokenBalance : getUserTokenBalance
+    getUserTokenBalance : getUserTokenBalance,
+    getUserAllTokenBalances : getUserAllTokenBalances
 }
