4b3013929a2f1b4a4236831de7d1339e14d7a54d
rrpanthe <rrpanthe@ncsu.edu>
2020-08-15 14:14:48 -0500

    getMarketMetrics

--- src/tokens/api.js
+++ src/tokens/api.js
@@ -51,6 +51,35 @@ async function getTokensDetails(query, limit, offset, index) {
   })
 }
 
+/**
+* @param {JSON} query Query for hive-engine
+* @param {Integer} limit limit
+* @param {Integer} offset offset
+* @param {Array} indexes indexes
+*/
+async function getMarketMetrics(query, limit, offset, index) {
+ let c = config.getConfig()
+ let tokensQuery = { id: 0, jsonrpc: "2.0", method: "find", params: { contract: "market", table: `metrics`, query: query, limit: limit, offset: offset, indexes: index } }
+ let res
+ let err
+ try {
+   res = await axios.post(c.engineRPC + "contracts", tokensQuery)
+ } catch (e) {
+   err = e
+ }
+ return new Promise((resolve, reject) => {
+   if (err) {
+     reject(new Error(err))
+     return
+   }
+   if (!res.data) {
+     reject(new Error("Invalid Query"))
+   }
+   resolve(res.data.result)
+ })
+}
+
 module.exports = {
-  getTokensDetails
+  getTokensDetails,
+  getMarketMetrics
 }
