c4e4bbf1e7cc902b083ec5421cf463015f981b1f
Rishi Panthee <rrpanthe@ncsu.edu>
2023-06-07 16:47:06 -0500

    Call

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ let config = require("./config.js");
 let token = require("./src/tokens/index.js");
 let nft = require("./src/nft/index.js");
 let history = require("./src/history/index.js");
+let call = require("./src/call/index.js");
 
 module.exports = {
     config,
     token,
     nft,
-    history
+    history,
+    call
 };
